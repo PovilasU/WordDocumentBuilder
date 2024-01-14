67ad5b6e82f19d4f07fb429e4715bf20ecd87b4f
PovilasU <povilas.urbonas85@gmail.com>
2024-01-14 11:41:47 +0000

    Added bookmarks

--- WordDocumentBuilder/Form1.cs
+++ WordDocumentBuilder/Form1.cs
@@ -39,22 +39,64 @@ namespace WordDocumentBuilder
             oWord = new Word.Application();
             oWord.Visible = true;
 
-            object oTemplate = "C:\\Projects\\C#\\WordDocumentBuilder\\word_documents\\template1.dot";
+          //  object oTemplate = "C:\\Projects\\C#\\WordDocumentBuilder\\word_documents\\template1_2.dot";
+            object oTemplate = "C:\\templates\\template1_4.dot";
+            // object oTemplate = "C:\\Projects\\C#\\WordDocumentBuilder\\word_documents\\Title.dot";
             oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing,
             ref oMissing, ref oMissing);
 
-            object oBookMark1 = "oBookMark1";
+            //Create Header
+            object oBookMark1 = "oBookMark1"; 
             oDoc.Bookmarks.get_Item(ref oBookMark1).Range.Text = "Some Text Here";
+            
+            object oBookMarkHeader = "oBookMarkHeader";
+            oDoc.Bookmarks.get_Item(ref oBookMarkHeader).Range.Text = "Sky Walker";
 
             //Insert a paragraph at the end of the document.
             Word.Paragraph oPara2;
             object oRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
             oPara2 = oDoc.Content.Paragraphs.Add(ref oRng);
-            oPara2.Range.Text = "Heading 2";
+            oPara2.Range.Text = "Heading 2Foo bar";
             oPara2.Format.SpaceAfter = 6;
+
+            object oStyleName = "Heading 1";
+            oPara2.Range.set_Style(ref oStyleName);
             oPara2.Range.InsertParagraphAfter();
 
 
+            //Insert another paragraph.
+            Word.Paragraph oPara3;
+            oRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oPara3 = oDoc.Content.Paragraphs.Add(ref oRng);
+            oPara3.Range.Text = "This is a sentence of normal text. Now here is a table:";
+            oPara3.Range.Font.Bold = 0;
+            oPara3.Format.SpaceAfter = 24;
+            object oStyleName1 = "boo";
+            oPara3.Range.set_Style(ref oStyleName1);
+            oPara3.Range.InsertParagraphAfter();
+
+            //Insert a 3 x 5 table, fill it with data, and make the first row
+            //bold and italic.
+            Word.Table oTable;
+            Word.Range wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oTable = oDoc.Tables.Add(wrdRng, 3, 5, ref oMissing, ref oMissing);
+            oTable.Range.ParagraphFormat.SpaceAfter = 6;
+            int r, c;
+            string strText;
+            for (r = 1; r <= 3; r++)
+                for (c = 1; c <= 5; c++)
+                {
+                    strText = "r" + r + "c" + c;
+                    oTable.Cell(r, c).Range.Text = strText;
+                }
+            oTable.Rows[1].Range.Font.Bold = 1;
+            oTable.Rows[1].Range.Font.Italic = 1;
+
+
+
+
+
+            //Inserting last Page
             Word.Range rng1 = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
 
             // Insert a page break at the end of the document.
@@ -71,13 +113,13 @@ namespace WordDocumentBuilder
 
 
 
-         
+
 
             // Save the document.
             try
             {
-                object fileName = @"C:\Projects\C#\WordDocumentBuilder\word_documents\newBar5.doc";
-                oDoc.SaveAs(ref fileName);
+                object fileName = @"C:\Projects\C#\WordDocumentBuilder\word_documents\newBar6.doc";
+                //  oDoc.SaveAs(ref fileName);
                 MessageBox.Show("File created successfully.");
             }
             catch (Exception ex)
@@ -85,8 +127,16 @@ namespace WordDocumentBuilder
                 MessageBox.Show("An error occurred: " + ex.Message);
             }
 
+
+            // Update the table of contents.
+            if (oDoc.TablesOfContents.Count > 0)
+            {
+                Word.TableOfContents toc = oDoc.TablesOfContents[1];
+                toc.Update();
+            }
+
             //Close this form.
-             this.Close();
+            this.Close();
 
             // Close the document.
             // doc1.Close();
